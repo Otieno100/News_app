06c17c09dcc7c80b8d5a568a2e017cc8a431f08e
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-03 17:55:28 +0300

    added main route for navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
 from flask import render_template
+
+import app
 from .import main
 from ..request import get_news
 # from .forms import ReviewForm
@@ -12,8 +14,7 @@ def index():
     popular_news = get_news()
     return render_template('index.html',articles = popular_news)
 
-
-@main.route('navbar.html')
+@main.route('/navbar')
 def navbar():
 
-  return render_template('navbar.html')
+    return render_template('navbar.html')
