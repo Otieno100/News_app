65c7ee685099c64d4a8297238c87c00bee4ffec2
Otieno100 <brian.masafu@student.moringaschool.com>
2022-05-03 15:01:35 +0300

    added an app route for navbar template and rendered the template to return navbar.html

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,13 @@ from ..models import Review
 
 @main.route('/')
 def index():
+
+
     popular_news = get_news()
     return render_template('index.html',articles = popular_news)
 
 
+@main.route('navbar.html')
+def navbar():
+
+  return render_template('navbar.html')
